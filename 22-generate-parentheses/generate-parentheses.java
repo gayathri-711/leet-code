83b93a9b11fe27b7664c9a83978c594b60eb86a2
gayathri-711 <gayatrisenthamarai@gmail.com>
2025-11-17 22:35:05 +0530

    Time: 0 ms (100.00%) | Memory: 44.2 MB (27.21%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.java
@@ -0,0 +1,31 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+        backtrack(result, new StringBuilder(), 0, 0, n);
+        return result;
+    }
+
+    private void backtrack(List<String> result, StringBuilder current,
+                           int open, int close, int n) {
+
+        // If the string is complete
+        if (current.length() == 2 * n) {
+            result.add(current.toString());
+            return;
+        }
+
+        // Add '(' if we still have opens left
+        if (open < n) {
+            current.append('(');
+            backtrack(result, current, open + 1, close, n);
+            current.deleteCharAt(current.length() - 1); // backtrack
+        }
+
+        // Add ')' only if it won't become invalid (close <= open)
+        if (close < open) {
+            current.append(')');
+            backtrack(result, current, open, close + 1, n);
+            current.deleteCharAt(current.length() - 1); // backtrack
+        }
+    }
+}
